a80c58dd650e490019e856e4665cb55df135e7d9
Soumyadeep Shome <43705072+Techie499@users.noreply.github.com>
2021-12-12 17:25:37 +0530

    Updated on 12-12-2021

---
+++ Inventory_Management_System/src/Swing/ConnectionFactory.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SWING;
+
+/**
+ *
+ * @author ARINDAM
+ */
+import java.sql.*;
+public class ConnectionFactory {
+    Connection cn=null;
+public Connection getConn()
+{
+	try
+	{
+		Class.forName("oracle.jdbc.driver.OracleDriver");//REGISTER AND LOAD THE JDBC DRIVER
+		  cn=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:ORCL","inv1","inv1");//ESTABLISH THE CONNECTION
+		
+	}
+	catch(ClassNotFoundException ce)
+	{
+		ce.printStackTrace();
+	}
+	catch(SQLException se)
+	{
+		se.printStackTrace();
+	}
+	return cn;
+}  
+}

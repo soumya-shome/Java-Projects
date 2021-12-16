7f6d0ee4406ddec19c742feafbca8344cd934f29
Soumyadeep Shome <43705072+Techie499@users.noreply.github.com>
2021-12-16 11:50:21 +0530

    Updated on 15-12-2021

--- Inventory_Management_System/src/ims/database/Conn.java
+++ Inventory_Management_System/src/ims/database/Conn.java
@@ -7,18 +7,17 @@ public class Conn {
     Connection cn=null;
     Statement st=null;
     ResultSet rs=null;
-    public Connection openConn()
+    public void openConn()
     {
 	try
 	{
             Class.forName("oracle.jdbc.driver.OracleDriver");//REGISTER AND LOAD THE JDBC DRIVER
-            cn=DriverManager.getConnection("jdbc:oracle:oci:@localhost:1521:xe","b22","b22");//ESTABLISH THE CONNECTION	
+            this.cn=DriverManager.getConnection("jdbc:oracle:oci:@localhost:1521:xe","b22","b22");//ESTABLISH THE CONNECTION	
 	}catch(ClassNotFoundException ce){
             ce.printStackTrace();
 	}catch(SQLException se){
             se.printStackTrace();
 	}
-	return cn;
     }
     
     public boolean checkLogin(String username,String password)
@@ -26,7 +25,7 @@ public class Conn {
         String sql="select * from registration where username='"+username+"' and password='"+password+"'";
         boolean flag=false;
         try{
-            st=cn.createStatement();//CREATE THE STATEMENT
+            st=this.cn.createStatement();//CREATE THE STATEMENT
             rs=st.executeQuery(sql);//EXECUTE THE QUERY AND RETRIEVE DATA INTO RESULTSET
             if(rs.next()){
                 flag=true;

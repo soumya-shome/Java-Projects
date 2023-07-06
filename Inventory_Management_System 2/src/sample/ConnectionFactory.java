9e462389a2b4a64938bbb0d6964636c2e4f1dde2
Soumyadeep Shome <soumyadeepshome99@gmail.com>
2023-07-06 20:17:43 +0530

    06-07-23

---
+++ Inventory_Management_System 2/src/sample/ConnectionFactory.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sample;
+
+/**
+ *
+ * @author ARINDAM
+ */
+import java.sql.*;
+public class ConnectionFactory {
+    Connection cn=null;
+public Connection getConn()
+{
+	try
+	{
+		Class.forName("oracle.jdbc.driver.OracleDriver");//REGISTER AND LOAD THE JDBC DRIVER
+		cn=DriverManager.getConnection("jdbc:oracle:oci:@localhost:1521:xe","b22","b22");//ESTABLISH THE CONNECTION
+		
+	}
+	catch(ClassNotFoundException ce)
+	{
+            System.out.println("Error !!!!!");
+		ce.printStackTrace();
+	}
+	catch(SQLException se)
+	{
+            System.out.println("SQL Error !!!");
+		se.printStackTrace();
+	}
+	return cn;
+}  
+}

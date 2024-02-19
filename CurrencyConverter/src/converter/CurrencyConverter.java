c1fe40eaccdefe1b2d2b088712a807adf6cac045
Soumyadeep Shome <soumyadeepshome99@gmail.com>
2024-02-20 00:48:42 +0530

    20-02-24

---
+++ CurrencyConverter/src/converter/CurrencyConverter.java
@@ -0,0 +1,17 @@
+package converter;
+
+public class CurrencyConverter {
+	int rupee = 63;
+	int dirham = 3;
+	int real = 3;
+	int chilean_peso = 595;
+	int mexican_peso = 18;
+	int _yen = 107;
+	int $australian = 2;
+	int dollar;
+	int Rupee = 63;
+	
+	void printCurrencies() {
+		System.out.
+	}
+}
